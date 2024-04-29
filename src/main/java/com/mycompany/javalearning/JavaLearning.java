6b439611fafc7b4335e9f43c24b204002c93ff7a
apollos <apollinaire.dabire@sonabel.bf>
2024-04-29 12:18:22 +0000

    Initialsation du projet

---
+++ src/main/java/com/mycompany/javalearning/JavaLearning.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javalearning;
+
+/**
+ *
+ * @author sonabel
+ */
+public class JavaLearning {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
