2f0b2a1c2bb0329b54abc2f5e2951a7bf0c4a98e
apollos <apollinaire.dabire@sonabel.bf>
2024-04-29 12:58:09 +0000

    Ajout de la méthode pour calculer la superficie d'un rectangle

--- src/main/java/com/mycompany/javalearning/JavaLearning.java
+++ src/main/java/com/mycompany/javalearning/JavaLearning.java
@@ -13,4 +13,14 @@ public class JavaLearning {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    /**
+     * Méthode pour calculer la superficie d'un rectange.
+     * @param longueur longueur du rectangle
+     * @param larageur largeur du rectangle
+     */
+    public float calculerSuperficieRectangle(final float longueur, final float larageur)
+    {
+        return longueur * larageur;
+    }
 }
